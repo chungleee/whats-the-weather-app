f40756b0e30d5be97552bfe3ff46e8c7a1924da1
Leon <leon.karho.chung@gmail.com>
2019-02-27 12:51:21 -0500

    add react react dom, testing index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log('bruh')
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+
+class App extends Component {
+	render() {
+		return (
+			<h1>hello world</h1>
+		)
+	}
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
