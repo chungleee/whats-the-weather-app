1c22b2717b80be14caa15334bcd759aae7b41aab
Leon <leon.karho.chung@gmail.com>
2019-02-28 15:39:16 -0500

    css and state

--- src/App.js
+++ src/App.js
@@ -2,10 +2,16 @@ import React, { Component } from 'react'
 import LocationDisplay from './components/LocationDisplay';
 import TemperatureDisplay from './components/TemperatureDisplay';
 
+import { darkSkyKey } from '../keys/apikey'
+
 class App extends Component {
+	state = {
+		
+	}
+
 	render() {
 		return (
-			<div className='background'>
+			<div className='container background'>
 				<LocationDisplay />
 				<TemperatureDisplay />
 			</div>
