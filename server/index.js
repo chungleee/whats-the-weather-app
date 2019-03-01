9b325b3b9045847df8fddcf5c75c0db1927bc551
Leon <leon.karho.chung@gmail.com>
2019-03-01 14:44:50 -0500

    set up morgan, cors to only accept one domain

--- server/index.js
+++ server/index.js
@@ -1,14 +1,24 @@
 const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
 const axios = require('axios')
 const { darkSkyKey } = require('../keys/apikey')
 
+const whitelist = ['http://localhost:8080']
+const options = {
+	origin: whitelist[0],
+	optionsSuccessStatus: 200
+}
+
+app.use(cors(options))
+app.use(morgan('tiny'))
+
 const url = 'https://api.darksky.net/forecast/0123456789abcdef9876543210fedcba/42.3601,-71.0589'
-app.get('/', (req, res) => {
+app.get('/weather', (req, res) => {
 	axios
 		.get(`https://api.darksky.net/forecast/${darkSkyKey}/42.3601,-71.0589`)
 		.then((response) => {
-			console.log(response.data.currently)
 			res.json(response.data.currently)
 		})
 		.catch((error) => {
