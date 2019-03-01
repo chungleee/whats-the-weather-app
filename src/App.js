3b5174d93e0d196168f110791562bbacf5997a7b
Leon <leon.karho.chung@gmail.com>
2019-03-01 15:45:06 -0500

    handleConvertTime

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import React, { Component } from 'react'
 import axios from 'axios'
+import moment from 'moment'
 import LocationDisplay from './components/LocationDisplay';
 import TemperatureDisplay from './components/TemperatureDisplay';
 
 class App extends Component {
 	state = {
-		currentWeather: {}
+		currentWeather: {},
+		currentTime: ''
 	}
 
 	componentDidMount() {
 		this.handleGetWeather()
+		this.handleConvertTime(this.state.currentWeather.time)
+	}
+
+	handleConvertTime = (number) => {
+		const day = moment(number).calendar()
+		console.log(day)
+		this.setState({
+			currentTime: day
+		})
 	}
 
 	handleGetWeather = () => {
@@ -26,11 +37,11 @@ class App extends Component {
 	}
 
 	render() {
-		const { currentWeather } = this.state
+		const { currentWeather, currentTime } = this.state
 
 		return (
 			<div className='container background'>
-				<LocationDisplay />
+				<LocationDisplay lastUpdated={currentTime} />
 				<TemperatureDisplay 
 					degree={ currentWeather.temperature } 
 					summary={ currentWeather.summary }
