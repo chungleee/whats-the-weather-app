3a901427cdb846eb166cdfeaf60dee56d5e9684c
Leon <leon.karho.chung@gmail.com>
2019-03-01 15:45:26 -0500

    render prop

--- src/components/LocationDisplay.js
+++ src/components/LocationDisplay.js
@@ -1,10 +1,10 @@
 import React from 'react'
 
-const LocationDisplay = () => {
+const LocationDisplay = ({ lastUpdated }) => {
 	return (
 		<div className='location-box'>
 			<h3 className='city'>Montreal</h3>
-			<h5 className='date'>Thursday, 28 Feb '19</h5>
+			<h5 className='date'>Last updated: { lastUpdated }</h5>
 		</div>
 	)
 }
