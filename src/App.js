32bf8fcc994cf03ed107c1741bc45fc2e2a47f0e
Leon <leon.karho.chung@gmail.com>
2019-03-01 15:17:17 -0500

    pass props

--- src/App.js
+++ src/App.js
@@ -4,14 +4,21 @@ import LocationDisplay from './components/LocationDisplay';
 import TemperatureDisplay from './components/TemperatureDisplay';
 
 class App extends Component {
+	state = {
+		currentWeather: {}
+	}
+
 	componentDidMount() {
 		this.handleGetWeather()
 	}
 
 	handleGetWeather = () => {
-		axios.get('http://localhost:3000/weather')
+		axios.get('http://localhost:3000/currently')
 			.then(response => {
-				console.log(response)
+				console.log(response.data)
+				this.setState({
+					currentWeather: response.data
+				})
 			})
 			.catch(error => {
 				console.log(error)
@@ -19,10 +26,15 @@ class App extends Component {
 	}
 
 	render() {
+		const { currentWeather } = this.state
+
 		return (
 			<div className='container background'>
 				<LocationDisplay />
-				<TemperatureDisplay />
+				<TemperatureDisplay 
+					degree={ currentWeather.temperature } 
+					summary={ currentWeather.summary }
+				/>
 			</div>
 		)
 	}
