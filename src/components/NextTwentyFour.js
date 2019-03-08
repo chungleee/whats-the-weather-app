dda99931b05155a75a31be8bcb5b34d3377a1ab5
Leon <leon.karho.chung@gmail.com>
2019-03-08 15:45:33 -0500

    class to className

--- src/components/NextTwentyFour.js
+++ src/components/NextTwentyFour.js
@@ -1,51 +1,52 @@
 import React from 'react'
 
 const NextTwentyFour = () => {
+
 	return (
 		<div className='hourly-block'>
 			<div className='hourly-block-items'>
 				<h3>-5&deg;</h3>
-				<h3><i class='uil uil-snowflake'></i></h3>
+				<h3><i className='uil uil-snowflake'></i></h3>
 				<h3>13:00</h3>
 			</div>
 			<div className='hourly-block-items'>
 				<h3>-5&deg;</h3>
-				<h3><i class='uil uil-snowflake'></i></h3>
+				<h3><i className='uil uil-snowflake'></i></h3>
 				<h3>13:00</h3>
 			</div>
 			<div className='hourly-block-items'>
 				<h3>-5&deg;</h3>
-				<h3><i class='uil uil-snowflake'></i></h3>
+				<h3><i className='uil uil-snowflake'></i></h3>
 				<h3>13:00</h3>
 			</div>
 			<div className='hourly-block-items'>
 				<h3>-5&deg;</h3>
-				<h3><i class='uil uil-snowflake'></i></h3>
+				<h3><i className='uil uil-snowflake'></i></h3>
 				<h3>13:00</h3>
 			</div>
 			<div className='hourly-block-items'>
 				<h3>-5&deg;</h3>
-				<h3><i class='uil uil-snowflake'></i></h3>
+				<h3><i className='uil uil-snowflake'></i></h3>
 				<h3>13:00</h3>
 			</div>
 			<div className='hourly-block-items'>
 				<h3>-5&deg;</h3>
-				<h3><i class='uil uil-snowflake'></i></h3>
+				<h3><i className='uil uil-snowflake'></i></h3>
 				<h3>13:00</h3>
 			</div>
 			<div className='hourly-block-items'>
 				<h3>-5&deg;</h3>
-				<h3><i class='uil uil-snowflake'></i></h3>
+				<h3><i className='uil uil-snowflake'></i></h3>
 				<h3>13:00</h3>
 			</div>
 			<div className='hourly-block-items'>
 				<h3>-5&deg;</h3>
-				<h3><i class='uil uil-snowflake'></i></h3>
+				<h3><i className='uil uil-snowflake'></i></h3>
 				<h3>13:00</h3>
 			</div>
 			<div className='hourly-block-items'>
 				<h3>-5&deg;</h3>
-				<h3><i class='uil uil-snowflake'></i></h3>
+				<h3><i className='uil uil-snowflake'></i></h3>
 				<h3>13:00</h3>
 			</div>
 		</div>
