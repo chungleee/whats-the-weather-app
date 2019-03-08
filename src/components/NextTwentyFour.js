7b42c54edc3c7a1d02171bcfbec84ba45a5bcc72
Leon <leon.karho.chung@gmail.com>
2019-03-08 18:54:46 -0500

    map hours array to display each hour

--- src/components/NextTwentyFour.js
+++ src/components/NextTwentyFour.js
@@ -1,56 +1,72 @@
 import React from 'react'
 
-const NextTwentyFour = () => {
-
+const NextTwentyFour = ({ hours }) => {
+	console.log(hours)
 	return (
 		<div className='hourly-block'>
-			<div className='hourly-block-items'>
-				<h3>-5&deg;</h3>
-				<h3><i className='uil uil-snowflake'></i></h3>
-				<h3>13:00</h3>
-			</div>
-			<div className='hourly-block-items'>
-				<h3>-5&deg;</h3>
-				<h3><i className='uil uil-snowflake'></i></h3>
-				<h3>13:00</h3>
-			</div>
-			<div className='hourly-block-items'>
-				<h3>-5&deg;</h3>
-				<h3><i className='uil uil-snowflake'></i></h3>
-				<h3>13:00</h3>
-			</div>
-			<div className='hourly-block-items'>
-				<h3>-5&deg;</h3>
-				<h3><i className='uil uil-snowflake'></i></h3>
-				<h3>13:00</h3>
-			</div>
-			<div className='hourly-block-items'>
-				<h3>-5&deg;</h3>
-				<h3><i className='uil uil-snowflake'></i></h3>
-				<h3>13:00</h3>
-			</div>
-			<div className='hourly-block-items'>
-				<h3>-5&deg;</h3>
-				<h3><i className='uil uil-snowflake'></i></h3>
-				<h3>13:00</h3>
-			</div>
-			<div className='hourly-block-items'>
-				<h3>-5&deg;</h3>
-				<h3><i className='uil uil-snowflake'></i></h3>
-				<h3>13:00</h3>
-			</div>
-			<div className='hourly-block-items'>
-				<h3>-5&deg;</h3>
-				<h3><i className='uil uil-snowflake'></i></h3>
-				<h3>13:00</h3>
-			</div>
-			<div className='hourly-block-items'>
-				<h3>-5&deg;</h3>
-				<h3><i className='uil uil-snowflake'></i></h3>
-				<h3>13:00</h3>
-			</div>
+			{
+				hours.map((hour, index) => {
+					return (
+						<div key={index} className='hourly-block-items'>
+ 							<h3>{hour.tempInC}&deg;</h3>
+ 							<h3><i className='uil uil-snowflake'></i></h3>
+							<h3>{hour.currentTime}:00</h3>
+						</div>
+					)
+				})
+			}
 		</div>
 	)
+
+	// return (
+	// 	<div className='hourly-block'>
+	// 		<div className='hourly-block-items'>
+	// 			<h3>-5&deg;</h3>
+	// 			<h3><i className='uil uil-snowflake'></i></h3>
+	// 			<h3>13:00</h3>
+	// 		</div>
+	// 		<div className='hourly-block-items'>
+	// 			<h3>-5&deg;</h3>
+	// 			<h3><i className='uil uil-snowflake'></i></h3>
+	// 			<h3>13:00</h3>
+	// 		</div>
+	// 		<div className='hourly-block-items'>
+	// 			<h3>-5&deg;</h3>
+	// 			<h3><i className='uil uil-snowflake'></i></h3>
+	// 			<h3>13:00</h3>
+	// 		</div>
+	// 		<div className='hourly-block-items'>
+	// 			<h3>-5&deg;</h3>
+	// 			<h3><i className='uil uil-snowflake'></i></h3>
+	// 			<h3>13:00</h3>
+	// 		</div>
+	// 		<div className='hourly-block-items'>
+	// 			<h3>-5&deg;</h3>
+	// 			<h3><i className='uil uil-snowflake'></i></h3>
+	// 			<h3>13:00</h3>
+	// 		</div>
+	// 		<div className='hourly-block-items'>
+	// 			<h3>-5&deg;</h3>
+	// 			<h3><i className='uil uil-snowflake'></i></h3>
+	// 			<h3>13:00</h3>
+	// 		</div>
+	// 		<div className='hourly-block-items'>
+	// 			<h3>-5&deg;</h3>
+	// 			<h3><i className='uil uil-snowflake'></i></h3>
+	// 			<h3>13:00</h3>
+	// 		</div>
+	// 		<div className='hourly-block-items'>
+	// 			<h3>-5&deg;</h3>
+	// 			<h3><i className='uil uil-snowflake'></i></h3>
+	// 			<h3>13:00</h3>
+	// 		</div>
+	// 		<div className='hourly-block-items'>
+	// 			<h3>-5&deg;</h3>
+	// 			<h3><i className='uil uil-snowflake'></i></h3>
+	// 			<h3>13:00</h3>
+	// 		</div>
+	// 	</div>
+	// )
 }
 
 export default NextTwentyFour
