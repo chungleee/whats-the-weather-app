7b57a138e424a2576a4f7eb1a14da0b898270ff8
Leon <leon.karho.chung@gmail.com>
2019-03-09 18:17:24 -0500

    fix

--- config/keys.js
+++ config/keys.js
@@ -1,8 +1,3 @@
-// module.exports = {
-// 	darkSkyKey: '64c96962b29d94d9b1dcb2e5683dac52',
-// 	googleMapsKey: 'AIzaSyCH3ygvXevD_Jslimrnbe7EgL9wlmZA2Ss'
-// }
-
 if(process.env.NODE_ENV === 'production') {
 	module.exports = require('./keys_prod')
 } else {
